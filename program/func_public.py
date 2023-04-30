5a7c37ec5d20f0f52a86394dcc9673a6cd69e45b
kvnbln <44892547+kvnbln@users.noreply.github.com>
2023-04-30 13:21:17 -0700

    stage 2 - cointegration

--- program/func_public.py
+++ program/func_public.py
@@ -10,46 +10,44 @@ from pprint import pprint
 ISO_TIMES = get_ISO_time()
 
 # get candles historical
-def get_candles_historical(client,market):
+def get_candles_historical(client, market):
 
-    # define output
-    close_prices = []
+  # Define output
+  close_prices = []
 
-    # extract historical price data for each time frame
-    for timeframe in ISO_TIMES.keys():
+  # Extract historical price data for each timeframe
+  for timeframe in ISO_TIMES.keys():
 
-      # confirm times needed
-      tf_obj = ISO_TIMES[timeframe]
-      from_iso = tf_obj["from_iso"]
-      to_iso = tf_obj["to_iso"]
+    # Confirm times needed
+    tf_obj = ISO_TIMES[timeframe]
+    from_iso = tf_obj["from_iso"]
+    to_iso = tf_obj["to_iso"]
 
-      # protect api
-      time.sleep(0.2)
+    # Protect rate limits
+    time.sleep(0.2)
 
-      # get data
-      candles = client.public.get_candles(
-         market=market,
-         resolution=RESOLUTION,
-         from_iso=from_iso,
-         to_iso=to_iso,
-         limit=100
-      )
+    # Get data
+    candles = client.public.get_candles(
+      market=market,
+      resolution=RESOLUTION,
+      from_iso=from_iso,
+      to_iso=to_iso,
+      limit=100
+    )
 
-      # structure data
-      for candle in candles.data["candles"]:
-         close_prices.append({"datetime": candle["startedAt"], market: candle["close"]})
+    # Structure data
+    for candle in candles.data["candles"]:
+      close_prices.append({"datetime": candle["startedAt"], market: candle["close"] })
 
-    # construct and return DataFrame
-    close_prices.reverse()
-    return close_prices
+  # Construct and return DataFrame
+  close_prices.reverse()
+  return close_prices
 
 
 # construct market prices
 def construct_market_prices(client):
 
-
-
-  # declare variables
+  # Declare variables
   tradeable_markets = []
   markets = client.public.get_markets()
 
@@ -57,28 +55,29 @@ def construct_market_prices(client):
   for market in markets.data["markets"].keys():
     market_info = markets.data["markets"][market]
     if market_info["status"] == "ONLINE" and market_info["type"] == "PERPETUAL":
-       tradeable_markets.append(market)
+      tradeable_markets.append(market)
 
-  # set initial datafram
+  # set initial dataframe
   close_prices = get_candles_historical(client, tradeable_markets[0])
-
   df = pd.DataFrame(close_prices)
   df.set_index("datetime", inplace=True)
 
-  # append other prices to dataframe - limit amount to loop through to save development time
-  for market in tradeable_markets[1:5]:
-     close_prices_add = get_candles_historical(client, market)
-     df_add = pd.DataFrame(close_prices_add)
-     df_add.set_index("datetime", inplace=True)
-     df = pd.merge(df, df_add, how="outer", on="datetime", copy=False)
-     del df_add
+  # Append other prices to DataFrame
+  # You can limit the amount to loop though here to save time in development
+  for market in tradeable_markets[1:]:
+    close_prices_add = get_candles_historical(client, market)
+    df_add = pd.DataFrame(close_prices_add)
+    df_add.set_index("datetime", inplace=True)
+    df = pd.merge(df, df_add, how="outer", on="datetime", copy=False)
+    del df_add
 
-  # Check any colums with NaNs
+    # Check any columns with NaNs
   nans = df.columns[df.isna().any()].tolist()
-  if len(nans) >0:
-    print("Dropping columns:")
+  if len(nans) > 0:
+    print("Dropping columns: ")
     print(nans)
     df.drop(columns=nans, inplace=True)
 
   # return result
-    return df
+  #print(df)
+  return df
